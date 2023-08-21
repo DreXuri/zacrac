3c182c5a2d984454c05b6997033f404b6c9a8f7d
Emmanuel Balogun <80977358+DreXuri@users.noreply.github.com>
2023-08-21 12:37:29 +0100

    app asset file updated

--- zacrac/lib/core/utils/assets_constant.dart
+++ zacrac/lib/core/utils/assets_constant.dart
@@ -10,12 +10,15 @@ class Assets {
   static const String appleIcon = '$_iconPath/apple.svg';
   static const String carIcon = '$_iconPath/car_icon.svg';
   static const String chartIcon = '$_iconPath/chart.svg';
+  static const String petronIcon = '$_iconPath/Patreon.svg';
   static const String settingsIcon = '$_iconPath/setting.svg';
-  static const String netflixIcon = '$_iconPath/netflix.svg';
+  static const String netflixIcon = '$_iconPath/Netflix.svg';
   static const String ticketIcon = '$_iconPath/ticket.svg';
   static const String receiptIcon = '$_iconPath/receipt_icon.svg';
   static const String spotifyIcon = '$_iconPath/spotify_icon.svg';
-  static const String arrowLeftIcon = '$_iconPath/arrow_left.svg';
+  static const String arrowBckIcon = '$_iconPath/arrow_left.svg';
+  static const String arrowDownIcon = '$_iconPath/arrow-down.svg';
+  static const String arrowRightIcon = '$_iconPath/arrow-right.svg';
   static const String calendarIcon = '$_iconPath/calendar.svg';
 
   //Images
